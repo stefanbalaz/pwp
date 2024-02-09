4982420c34518a4ae55561510f420e2bc21e01d9
stefanbalazclear <stefanbalaz@hotmail.com>
2024-02-09 14:20:02 +0100

    responsiveness

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,20 +5,22 @@ function Profile() {
     <div>
       <h3>PROFILE</h3>
       <div className="content_block">
-        Driven by a passion for innovation and a proven track record of success
-        in diverse roles across the online business landscape, I am now
-        embarking on an exciting transition into the field of software
-        development.
-      </div>
-      <div className="content_block">
-        Leveraging my experience and proficiency in product development,
-        stakeholder relations, and strategic management, I have actively
-        utilized tools like Jira Confluence to streamline workflows, enhance
-        collaboration, and drive project success.
-      </div>
-      <div className="content_block">
-        I am eager to apply my expertise and motivation, leveraging these
-        skills, to thrive in this dynamic environment.
+        <p>
+          Driven by a passion for innovation and a proven track record of
+          success in diverse roles across the online business landscape, I am
+          now embarking on an exciting transition into the field of software
+          development.
+        </p>
+        <p>
+          Leveraging my experience and proficiency in product development,
+          stakeholder relations, and strategic management, I have actively
+          utilized tools like Jira Confluence to streamline workflows, enhance
+          collaboration, and drive project success.
+        </p>
+        <p>
+          I am eager to apply my expertise and motivation, leveraging these
+          skills, to thrive in this dynamic environment.
+        </p>
       </div>
     </div>
   );
