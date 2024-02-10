08828cc9a3a523f039622329ef971131d8022bf7
stefanbalazclear <stefanbalaz@hotmail.com>
2024-02-10 15:50:23 +0100

    sencond portfolio project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,6 +5,8 @@ function Portfolio() {
     <div>
       <h3>PORTFOLIO</h3>
 
+      {/* AMATE ADMIN DASHBOARD */}
+
       <div className="content_block">
         {/* HEADLINE */}
         <div className="card_headline">AMATE Admin Dashboard</div>
@@ -85,6 +87,87 @@ function Portfolio() {
             </div>
             {/* BUTTON */}
             <button className="btn custom-button">Visit</button>
+          </div>
+        </div>
+      </div>
+
+      {/* DELIVERY PRICE CALCULATOR */}
+
+      <div className="content_block">
+        {/* HEADLINE */}
+        <div className="card_headline">Delivery Price Calculator</div>
+
+        <div className="portfolio_card">
+          <div className="card_image">
+            {/* IMAGE */}
+            <div className="product-image">
+              <a href="#">
+                <img
+                  src="Delivery_Price_Calculator.png"
+                  alt="Delivery price calculator"
+                />
+              </a>
+              <div className="hover-img">
+                <a href="#">
+                  <img
+                    src="Delivery_Price_Calculator.png"
+                    alt="Delivery price calculator"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="card_content">
+            {/* ICONS */}
+            <div className="card_icons">
+              <div className="icon_element">
+                <div className="tooltip">React</div>
+                <img
+                  src="../../React_Logo.png"
+                  className="tech_icon"
+                  alt="React Logo"
+                />
+                <div className="tech_name">React</div>
+              </div>
+              <div className="icon_element">
+                <div className="tooltip">Bootstrap</div>
+                <img
+                  src="../../Bootstrap_Logo.png"
+                  className="tech_icon"
+                  alt="Bootstrap Logo"
+                />
+                <div className="tech_name">Bootstrap</div>
+              </div>
+            </div>
+            {/* DESCRIPTION */}
+            <div className="card_description">
+              <ul className="card_description_list">
+                <li>
+                  Online tool for the drink delivery service designed for mobile
+                  devices.
+                </li>
+                <li>
+                  Streamlining pricing calculations and enhancing operational
+                  efficiency.
+                </li>
+                <li>
+                  Key features: precise calculation based on sold and received
+                  items, sending detailed calculations to customer via email.
+                </li>
+              </ul>
+            </div>
+            {/* BUTTON */}
+            <button
+              className="btn custom-button"
+              onClick={() =>
+                window.open(
+                  "https://courageous-pavlova-6c49c4.netlify.app/",
+                  "_blank"
+                )
+              }
+            >
+              Visit
+            </button>
           </div>
         </div>
       </div>
