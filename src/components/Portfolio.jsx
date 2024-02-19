47d3f90b78061ecabc929e965308556efed2e6fc
stefanbalazclear <stefanbalaz@hotmail.com>
2024-02-19 21:57:20 +0100

    portfolio details added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Portfolio() {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
   return (
     <div>
       <h3>PORTFOLIO</h3>
@@ -79,8 +85,53 @@ function Portfolio() {
                 </li>
               </ul>
             </div>
+
+            {/* Toggle "More Details" visibility */}
+            <div className="more-details-link" onClick={toggleDetails}>
+              {showDetails ? "Less Details" : "More Details"}{" "}
+              {showDetails ? "▲" : "▼"}
+            </div>
+
+            {/* Expandable "More Details" link */}
+            {showDetails && (
+              <div className="expandable-content">
+                <div style={{ fontWeight: "bold" }}>
+                  <p> WORK IN PROGRESS</p>
+                </div>
+                <div style={{ textDecoration: "underline" }}>
+                  <p>Working in current version:</p>
+                </div>
+                <ul className="card_description_list">
+                  <li>Sign-up, Log-in, Log-out</li>
+                  <li>Order overview</li>
+                  <li>Order status change, Payment status change</li>
+                  <li>Invoice creation</li>
+                  <li>Expandable row with order details (missing data)</li>
+                </ul>
+              </div>
+            )}
+
             {/* BUTTON */}
-            <button className="btn custom-button">Visit</button>
+            <div className="tooltip-container">
+              <div className="tooltip">
+                Username: guest <br />
+                Password: guest
+                <br />
+                <br />
+                After login, wait briefly for backend activation.
+              </div>
+              <button
+                className="btn custom-button"
+                onClick={() =>
+                  window.open(
+                    "https://delivery-price-calculator.netlify.app/",
+                    "_blank"
+                  )
+                }
+              >
+                Visit
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -121,6 +172,15 @@ function Portfolio() {
                 <div className="tech_name">React</div>
               </div>
               <div className="icon_element">
+                <div className="tooltip">Typescript</div>
+                <img
+                  src="../../Typescript_Logo.png"
+                  className="tech_icon"
+                  alt="Typescript Logo"
+                />
+                <div className="tech_name">Typescript</div>
+              </div>
+              <div className="icon_element">
                 <div className="tooltip">Bootstrap</div>
                 <img
                   src="../../Bootstrap_Logo.png"
@@ -148,6 +208,8 @@ function Portfolio() {
               </ul>
             </div>
             {/* BUTTON */}
+            {/*   <div className="tooltip-container">
+              <div className="tooltip">Test</div> */}
             <button
               className="btn custom-button"
               onClick={() =>
@@ -159,6 +221,68 @@ function Portfolio() {
             >
               Visit
             </button>
+            {/*  </div> */}
+          </div>
+        </div>
+      </div>
+
+      {/* PERSONANAL WEBSITE */}
+
+      <div className="content_block">
+        {/* HEADLINE */}
+        <div className="card_headline">Personal Website</div>
+
+        <div className="portfolio_card">
+          <div className="card_image">
+            {/* IMAGE */}
+            <div className="product-image">
+              <img src="Personal_Website.png" alt="Personal Website" />
+
+              <div className="hover-img">
+                <img src="Personal_Website.png" alt="Personal Website" />
+              </div>
+            </div>
+          </div>
+          <div className="card_content">
+            {/* ICONS */}
+            <div className="card_icons">
+              <div className="icon_element">
+                <div className="tooltip">React</div>
+                <img
+                  src="../../React_Logo.png"
+                  className="tech_icon"
+                  alt="React Logo"
+                />
+                <div className="tech_name">React</div>
+              </div>
+              <div className="icon_element">
+                <div className="tooltip">HTML</div>
+                <img
+                  src="../../HTML5_Logo.png"
+                  className="tech_icon"
+                  alt="HTML Logo"
+                />
+                <div className="tech_name">HTML</div>
+              </div>
+              <div className="icon_element">
+                <div className="tooltip">CSS</div>
+                <img
+                  src="../../CSS_Logo.png"
+                  className="tech_icon"
+                  alt="CSS Logo"
+                />
+                <div className="tech_name">CSS</div>
+              </div>
+            </div>
+            {/* DESCRIPTION */}
+            <div className="card_description">
+              <ul className="card_description_list">
+                <li>
+                  Highlighting diverse projects, CV, and skills to provide a
+                  comprehensive overview of achievements and capabilities.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
