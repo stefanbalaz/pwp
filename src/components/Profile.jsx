7533e5b39a5885449d18a879eeb03bb2ca892573
stefanbalazclear <stefanbalaz@hotmail.com>
2024-02-21 15:28:18 +0100

    text profile update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,20 +6,25 @@ function Profile() {
       <h3>PROFILE</h3>
       <div className="content_block">
         <p>
+          With 16 years of experience in the online industry and entrepreneurial
+          spirit, my passion lies in building and optimizing complex digital
+          products and processes. I've held various roles, primarily as a
+          Product Manager, Operations Manager, Business Development as well as
+          eCommerce Manager.
+        </p>
+        <p>
           Driven by a passion for innovation and a proven track record of
           success in diverse roles across the online business landscape, I am
           now embarking on an exciting transition into the field of software
-          development.
+          development. Leveraging my experience and proficiency in product
+          development, stakeholder relations, and strategic management, I have
+          actively utilized various tools and methods to streamline workflows,
+          enhance collaboration, and drive project success.
         </p>
         <p>
-          Leveraging my experience and proficiency in product development,
-          stakeholder relations, and strategic management, I have actively
-          utilized tools like Jira Confluence to streamline workflows, enhance
-          collaboration, and drive project success.
-        </p>
-        <p>
-          I am eager to apply my expertise and motivation, leveraging these
-          skills, to thrive in this dynamic environment.
+          Now transitioning to software development, I'm eager to apply my
+          expertise and motivation, leveraging these skills, to thrive in this
+          dynamic environment.
         </p>
       </div>
     </div>
